477d81ca40c170d2222332c871eea18e8121650a
Pirozhki <Pirozhki123@users.noreply.github.com>
2023-01-02 23:44:57 +0000

    [add]indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -15,6 +15,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  # def destroy
+  #   book = Book.find(params[:id])
+  #   book.destroy
+  #   redirect_to '/books'
+  # end
 
   private
 
